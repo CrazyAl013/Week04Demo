3298f996b677778a591fc21ac60595399a3a7bfb
cprg352 <cprg352@demongeek.com>
2020-09-24 14:12:17 -0600

    Converted application into a full MVC pattern.

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -5,16 +5,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Person;
 
 /**
  *
  * @author awarsyle
  */
 public class HelloWorldServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
-                .forward(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp").forward(request, response);
     }
 
     @Override
@@ -22,25 +23,26 @@ public class HelloWorldServlet extends HttpServlet {
         // get the two parameters from the POST request
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
-        
-        // set the attributes for the JSP
-        request.setAttribute("firstname", firstname);
-        request.setAttribute("lastname", lastname);
-        
+
         // validation: if the parameters don't exist or are empty, show the first page again
         if (firstname == null || firstname.equals("") || lastname == null || lastname.equals("")) {
+            // set the attributes for the JSP
+            request.setAttribute("firstname", firstname);
+            request.setAttribute("lastname", lastname);
+            
             // set an attribute for a message
             request.setAttribute("message", "Invalid entry. Please enter both your first and last names.");
-            
+
             // forward the request and response objects to the JSP
             // display the form again
-            getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
-                    .forward(request, response);
+            getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp").forward(request, response);
             return; // very important!
         }
 
+        Person person = new Person(firstname, lastname);
+        request.setAttribute("person", person);
+
         // display the second page
-        getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
-                .forward(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp").forward(request, response);
     }
 }
